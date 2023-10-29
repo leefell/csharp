821df6a4370b30d22c0dc069d154b9939503142b
leefell <alexandreaug21@gmail.com>
2023-10-29 14:53:57 -0300

    Create SaidaDeDados.cs

---
+++ Sintaxe/SaidaDeDados.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade = 32;
+            double saldo = 10.35234;
+            string nome = "Rafael";
+
+            Console.Write("Bom dia");
+            Console.WriteLine("Boa tarde");
+            Console.WriteLine("Boa noite");
+            Console.WriteLine("----------------------------");
+
+            Console.WriteLine(saldo.ToString("F2")); // Arredonda e imprime com duas casas decimais.
+            Console.WriteLine(saldo.ToString("F4")); // 4 casas decimais
+            Console.WriteLine(saldo.ToString("F4", CultureInfo.InvariantCulture)); // O InvariantCulture vai desconsiderar qualquer separação específica de pais
+            Console.WriteLine("----------------------------");
+
+            // PlaceHolder 
+
+            Console.WriteLine("{0} tem {1} anos e tem saldo igual a {2:F2} reais.", nome, idade, saldo);
+
+            // Interpolação
+
+            Console.WriteLine($"{nome} tem {idade} anos e tem saldo igual a {saldo:F2} reais.");
+
+            // Concatenação de String
+
+            Console.WriteLine(nome + " tem " + idade + " anos e tem saldo igual a " + saldo.ToString("F2", CultureInfo.InvariantCulture) + " reais.");
+        }
+    }
+}
