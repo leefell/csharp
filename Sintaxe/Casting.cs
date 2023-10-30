9f87b5bff16eb61a19abd52cf782a46f3b5f2e1d
leefell <alexandreaug21@gmail.com>
2023-10-30 11:05:36 -0300

    Create Casting.cs

---
+++ Sintaxe/Casting.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.AccessControl;
+
+namespace Casting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Casting -> conversão explícita de tipos
+            // Pode jogar o conteudo de 8 bytes na variavel de 4, e o dev se responsabiliza por alguma perda de informação 
+            // Se fizer o casting de double para int, ele vai arredondar, 5.1 -> 5
+
+            double a;
+            int b;
+
+            a = 5.1;
+            b = (int)a; 
+            Console.WriteLine(b);
+
+
+            int c = 5;
+            int d = 2;
+            double resultado = (double)c / d; // Dessa maneira não vai descartar as casas decimais.
+            Console.WriteLine(resultado);
+
+
+        }
+    }
+}
