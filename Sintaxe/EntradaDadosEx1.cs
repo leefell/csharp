6ff6f2395ad37d07f7e86c6d7994fb4c7705391e
leefell <alexandreaug21@gmail.com>
2023-10-30 14:04:24 -0300

    Create EntradaDadosEx1.cs

---
+++ Sintaxe/EntradaDadosEx1.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+
+namespace EntradaDadosEx1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entre com seu nome completo: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Quantos quartos tem na sua casa?");
+            int quartos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Entre com o preço de um produto: ");
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Entre com seu último nome, idade e altura (mesma linha): ");
+            string[] array = Console.ReadLine().Split(' ');
+            string ultimoNome = array[0];
+            int idade = int.Parse(array[1]);
+            float altura = float.Parse(array[2], CultureInfo.InvariantCulture);
+
+            Console.WriteLine(nome);
+            Console.WriteLine(quartos);
+            Console.WriteLine(preco.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine(ultimoNome);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
