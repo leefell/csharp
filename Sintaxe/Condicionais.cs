61b3e3c84ab9fcc74b9397fa1dc1bf8380d5c116
leefell <alexandreaug21@gmail.com>
2023-10-31 10:18:32 -0300

    Create Condicionais.cs

---
+++ Sintaxe/Condicionais.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml;
+
+namespace IfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Qual a hora atual? ");
+            int hora = int.Parse(Console.ReadLine());
+
+            if(hora < 12)
+            {
+                Console.WriteLine("Bom dia!");
+
+            }
+            else if(hora >= 12 && hora < 18)
+            {
+                Console.WriteLine("Boa tarde.");
+            }
+            else
+            {
+                Console.WriteLine("Boa noite!");
+            }
+
+            //Console.WriteLine("Digite um número inteiro: ");
+            //int x = int.Parse(Console.ReadLine());
+
+            //if(x % 2 == 0)
+            //{
+            //    Console.WriteLine("Par.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Impar.");
+            //}
+
+        }
+    }
+}
