46e2079e47aea3db99e83df628cda85fc0eef96f
leefell <alexandreaug21@gmail.com>
2023-11-01 14:07:22 -0300

    Create Funcao.cs

---
+++ Sintaxe/Funcao.cs
@@ -0,0 +1,37 @@
+﻿namespace Funcoes
+{
+    internal class Program
+    {     
+        static void Main(string[] args)
+        {
+            // Em OO funções recebem o nome de métodos.
+
+            Console.WriteLine("Digite 3 números: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            double resultado = Maior(n1, n2, n3);
+            double resultado2 = Maior(700, 231, 1000);
+
+            Console.WriteLine("Maior = " + resultado);
+            Console.WriteLine("Maior = " + resultado2);
+        }
+
+        static int Maior(int a, int b, int c)
+        {
+            if (a > b && a > c)
+            {
+                return a;
+            }
+            else if (b > c)
+            {
+                return b;
+            }
+            else
+            {
+                return c;
+            }
+        }
+    }
+}
