70e9fa8802d6edc7736c818a1f75099419b60eac
leefell <alexandreaug21@gmail.com>
2023-11-03 09:56:30 -0300

    Create estFor.cs

---
+++ Sintaxe/estFor.cs
@@ -0,0 +1,24 @@
+﻿namespace est.For
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Digite a quantidade de números inteiros: ");
+            int qtd = int.Parse(Console.ReadLine());
+
+            int soma = 0;
+            for(int i = 1; i <= qtd; i++)
+            {
+                Console.Write("Valor #{0}: ", i);
+                int valor = int.Parse(Console.ReadLine());
+                soma += valor;
+            }
+
+            Console.WriteLine("Soma = " + soma);
+
+        }
+
+    }
+}
