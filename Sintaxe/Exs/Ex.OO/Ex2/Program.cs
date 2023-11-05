4ba64ae76f571a63a2a80b44f21e038683e5de70
leefell <alexandreaug21@gmail.com>
2023-11-05 18:54:42 -0300

    push push

---
+++ Sintaxe/Exs/Ex.OO/Ex2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+namespace Ex2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Funcionario p1 = new Funcionario();
+            Funcionario p2 = new Funcionario();
+            double salarioMedio = 0;
+
+            Console.WriteLine("Dados do Funcionário 1: ");
+            Console.Write("Nome: ");
+            p1.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            p1.salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Dados do Funcionário 2: ");
+            Console.Write("Nome: ");
+            p2.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            p2.salario =  double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salarioMedio = (p1.salario + p2.salario) / 2;
+
+            Console.WriteLine("Salario médio: " + salarioMedio.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
