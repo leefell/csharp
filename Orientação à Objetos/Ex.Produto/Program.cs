de833477b9fec0d814d4169511ddafac54d712bc
leefell <alexandreaug21@gmail.com>
2023-11-05 21:49:24 -0300

    Add exs

---
+++ Orientação à Objetos/Ex.Produto/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Runtime;
+
+namespace Ex.Produto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto produto = new Produto();
+
+            Console.WriteLine("Digite os dados do produto.");
+            Console.Write("Nome: ");
+            produto.Nome = Console.ReadLine();
+            Console.Write("Preço: ");
+            produto.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantidade no estoque: ");
+            produto.Quantidade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.WriteLine("\nDados do produto: " + produto);
+
+            Console.Write("\nDigite a quantidade a ser adicionada ao estoque: ");
+            int qtd = int.Parse(Console.ReadLine());
+            produto.AdicionarProdutos(qtd);
+            Console.WriteLine("Dados atualizados: " + produto);
+
+            Console.Write("\nDigite a quantidade a ser removida do estoque: ");
+            qtd = int.Parse(Console.ReadLine());
+            produto.RemoverProdutos(qtd);
+            Console.WriteLine("Dados atualizados: " + produto);
+        }
+    }
+}
