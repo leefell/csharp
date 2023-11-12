32b4251e9cc062eadb375ef2d8f53ecd4c5dfc93
leefell <alexandreaug21@gmail.com>
2023-11-12 11:05:44 -0300

    Propriedades

---
+++ Orientação à Objetos/Propriedades/Produto.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+
+namespace Propriedades
+{
+    internal class Produto
+    {
+
+       
+
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome + ", $" + _preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + _quantidade
+                + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+        public string Nome // Implementação de uma propriedade
+        {
+            get { return _nome; }
+            set {
+                if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        }
+
+        public double Preco
+        {
+            get { return _preco; }
+        }
+        
+        public int Quantidade
+        {
+            get { return _quantidade; }
+        }
+    }
+}
---
+++ Orientação à Objetos/Propriedades/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Propriedades
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto produto = new Produto("Garrafa", 500.00, 10);
+
+            produto.Nome = "Garrafa de Água";
+
+            Console.WriteLine(produto.Nome);
+            Console.WriteLine(produto.Preco);
+            Console.WriteLine(produto.Quantidade);
+        }
+    }
+}
