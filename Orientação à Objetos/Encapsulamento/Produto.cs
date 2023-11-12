15c05bf7899bd01b7b31aa8d294be0d22ba32c3b
leefell <alexandreaug21@gmail.com>
2023-11-12 10:49:34 -0300

    Encapsulamento

---
+++ Orientação à Objetos/Encapsulamento/Produto.cs
@@ -0,0 +1,63 @@
+﻿using System.Globalization;
+namespace Encapsulamento
+{
+    internal class Produto
+    {
+
+        // Atributo privado tem esse padrao de nome _minisculo
+
+        private string _nome;
+        private double _preco;
+        private int _quantidade;
+
+        public Produto()
+        {
+
+        }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            _preco = preco;
+            _quantidade = quantidade;
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return _preco * _quantidade;
+        }
+
+        public void AdicionarProdutos(int quantidade)
+        {
+            _quantidade += quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome + ", $" + _preco.ToString("F2", CultureInfo.InvariantCulture) + ", " + _quantidade
+                + " unidades, Total: $ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+        public string GetNome()
+        {
+            return _nome;
+        }
+
+        public void SetNome(string nome)
+        {
+            if (nome != null && nome.Length > 1) // Esse set só vai aceitar o nome se ele nao for nulo e
+            {                                   // possuir mais de 1 caracter
+                _nome = nome;
+            }
+        }
+
+        public double GetPreco()
+        {
+            return _preco;
+        }
+
+        public int GetQuantidade()
+        {
+            return _quantidade;
+        }
+    }
+}
---
+++ Orientação à Objetos/Encapsulamento/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Encapsulamento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV", 500.00, 10);
+
+            p.SetNome("T"); // Set para alterar
+            Console.WriteLine(p.GetNome()); // Get para mostrar
+            Console.WriteLine(p.GetPreco());
+        }
+    }
+}
