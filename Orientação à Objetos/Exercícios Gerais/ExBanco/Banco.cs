1b13e98e7332fbafe44fbbb35fac3207ed418471
leefell <alexandreaug21@gmail.com>
2023-11-13 13:27:26 -0300

    Exercicio Banco

---
+++ Orientação à Objetos/Exercícios Gerais/ExBanco/Banco.cs
@@ -0,0 +1,47 @@
+﻿using System.Globalization;
+using System.Reflection.Metadata;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+
+namespace ExBanco
+{
+    internal class Banco
+    {
+
+        public int Numero {  get; private set; }
+        public string Titular { get; set; }
+        public double Saldo {  get; private set; }
+
+
+        public Banco(int numero, string titular)
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        public Banco(int numero, string titular, double depositoInicial) : this(numero, titular)
+        {
+            Deposito(depositoInicial); // É interessante fazer dessa maneira porque se a logica do Deposito
+                                       // um dia mudar, ele ja vai alterar aqui
+        }
+
+
+       public void Deposito(double quantia)
+        {
+
+            Saldo += quantia;
+
+        }
+
+        public void Saque(double quantia)
+        {
+            Saldo -= quantia + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return "Conta " + Numero + ", Titular: " + Titular +
+                ", Saldo: $" + Saldo.ToString("F2", CultureInfo.InvariantCulture);  
+        }
+    }
+}
---
+++ Orientação à Objetos/Exercícios Gerais/ExBanco/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Globalization;
+namespace ExBanco
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Banco pessoa;
+
+            Console.Write("Insira o número da conta: ");
+            int numero = int.Parse(Console.ReadLine());
+
+            Console.Write("Insira o Titular da conta: ");
+            string titular = Console.ReadLine();
+
+            Console.Write("Haverá depósito inicial? (s/n): ");
+            char opc = char.Parse(Console.ReadLine());
+            if(opc == 's' || opc == 'S')
+            {
+                Console.Write("Digite o valor do depósito incial: ");
+                double depositoInicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                pessoa = new Banco(numero, titular, depositoInicial);
+            }
+            else
+            {
+                pessoa = new Banco(numero, titular);
+            }
+
+            Console.WriteLine("\nDados da conta: ");
+            Console.WriteLine(pessoa);
+
+            Console.Write("Insira um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            pessoa.Deposito(deposito);
+
+            Console.WriteLine("\nDados Atualizados!");
+            Console.WriteLine(pessoa);
+
+            Console.WriteLine("Existe uma taxa de $5 por saque.");
+            Console.Write("Insira um valor para saque: ");
+            double saque = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            pessoa.Saque(saque);
+
+            Console.WriteLine("\nDados Atualizados!");
+            Console.WriteLine(pessoa);
+        }
+    }
+}
