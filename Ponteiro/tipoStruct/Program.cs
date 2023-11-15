ccaa52fd603f4e896fe6fc75eb13e29cc8c5dbda
leefell <alexandreaug21@gmail.com>
2023-11-15 09:52:16 -0300

    Introduzindo o conceito de Ponteiros

---
+++ Ponteiro/tipoStruct/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace tipoStruct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Point p; // mesmo sendo struct deve ser inicializada
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+            p = new Point();
+            Console.WriteLine(p);
+
+        }
+    }
+}
