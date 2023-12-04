d42c031c49194bd8578ce18ed03d026377e0cbbc
leefell <alexandreaug21@gmail.com>
2023-12-04 07:40:42 -0300

    Array

---
+++ Array/exemploArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace exemploArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o tamanho do array para alocação estática: ");
+            int n =  int.Parse(Console.ReadLine());
+
+            double[] array = new double[n];
+            double soma = 0.0;
+
+            for (int i  = 0; i < array.Length; i++)
+            {
+                Console.WriteLine("Digite a altura " + (i + 1) + ": " );
+                array[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                soma += array[i];
+            }
+
+            double media = soma/n;
+            Console.WriteLine("Altura média: " + media.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
