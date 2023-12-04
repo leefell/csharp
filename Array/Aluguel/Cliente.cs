99448de80963c0db15d90165adae0049c4f26faf
leefell <alexandreaug21@gmail.com>
2023-12-04 09:15:59 -0300

    Exercicio controle de aluguel

---
+++ Array/Aluguel/Cliente.cs
@@ -0,0 +1,21 @@
+﻿namespace Aluguel
+{
+    internal class Cliente
+    {
+
+        public string Nome { get; set; }
+        public string Email{ get; set; }
+
+        public Cliente(string nome, string email)
+        {
+            Nome = nome;
+            Email = email;
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " + Email;
+        }
+
+    }
+}
---
+++ Array/Aluguel/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Aluguel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cliente[] pessoa = new Cliente[10];
+
+            Console.Write("Quantos quartos serão alugados: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            for(int i = 1; i <= n; i++)
+            {
+                Console.WriteLine("\nAluguel #" + (i));
+                Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Quarto: ");
+                int quarto = int.Parse(Console.ReadLine());
+                pessoa[quarto] = new Cliente(nome, email);
+            }
+
+            Console.WriteLine("\nQuartos ocupados: ");
+            for(int i = 0; i < 10; i++)
+            {
+                if (pessoa[i] != null)
+                {
+                    Console.WriteLine(i + ": " + pessoa[i]);
+                }
+            }
+
+        }
+    }
+}
