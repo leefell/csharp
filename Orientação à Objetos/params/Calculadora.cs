ddd22329bb9ae522eca740f4c640f8bf970f5214
leefell <alexandreaug21@gmail.com>
2023-12-07 18:43:55 -0300

    params

---
+++ Orientação à Objetos/params/Calculadora.cs
@@ -0,0 +1,19 @@
+﻿namespace modificadorParametros
+{
+    internal class Calculadora
+    {
+
+        // Definição de função com parametros variaveis
+
+        public static int Soma(params int[] numeros)
+        {
+            int sum = 0;
+            for(int i = 0; i < numeros.Length; i++)
+            {
+                sum += numeros[i];
+            }
+            return sum;
+        }
+
+    }
+}
---
+++ Orientação à Objetos/params/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace modificadorParametros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int soma1 = Calculadora.Soma(1, 2, 3, 4, 5, 6, 7);
+            int soma2 = Calculadora.Soma(2, 4, 3 );
+
+            Console.WriteLine(soma1);
+            Console.WriteLine(soma2);
+
+        }
+    }
+}
