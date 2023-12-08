ac844dbd58d41e0d5c221ad9629b242bc1f83b80
leefell <alexandreaug21@gmail.com>
2023-12-08 10:05:27 -0300

    mais mod

---
+++ Orientação à Objetos/params/modRef/Calculadora.cs
@@ -0,0 +1,18 @@
+﻿namespace modRef
+{
+    internal class Calculadora
+    {
+        // Com o 'ref' o parametro x da funcao se torna uma referencia para a variavel original
+        // então quando é executado ele ja modifica na variavel original
+        public static void TriplicarREF(ref int x)
+        {
+            x = x * 3;
+        }
+
+        public static void TriplicarOUT(int origem, out int resultado)
+        {
+            resultado = origem * 3;
+        }
+
+    }
+}
---
+++ Orientação à Objetos/params/modRef/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace modRef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //code smells (design ruim)
+            Console.WriteLine("Por ref");
+            int a = 10;
+            Calculadora.TriplicarREF(ref a);
+            Console.WriteLine(a);
+
+            Console.WriteLine("\nPor out");
+            int b = 10;
+            int triplicar; // essa é a variavel nao inicializada
+            Calculadora.TriplicarOUT(b, out triplicar);
+            Console.WriteLine(triplicar);
+        }
+    }
+}
