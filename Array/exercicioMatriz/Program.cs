0277766de8ce97be1ecc43191bc33adf78a8a810
leefell <alexandreaug21@gmail.com>
2023-12-09 15:41:57 -0300

    matriz

---
+++ Array/exercicioMatriz/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace exercicioMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ordem da matriz quadradada: ");
+            int tamanho = int.Parse(Console.ReadLine());
+
+            int[,] matriz = new int[tamanho, tamanho];
+
+            Console.WriteLine("Preencher a matriz: ");
+            for (int i = 0; i < tamanho; i++)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    Console.Write("Elemento " + i + ", " + j + ": ");
+                    matriz[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("Matriz preenchida: ");
+            for (int i = 0; i < tamanho; i++)
+            {
+                for (int j = 0; j < tamanho; j++)
+                {
+                    Console.Write(matriz[i, j] + "\t");
+                }
+                Console.WriteLine(); 
+            }
+
+            Console.WriteLine("Diagonal Principal: ");
+            for(int i = 0; i < tamanho; i++)
+            {
+                Console.Write(matriz[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            int contador = 0;
+            for(int i = 0; i < tamanho; i++)
+            {
+                for(int j = 0; j < tamanho; j++)
+                {
+                    if (matriz[i,j] < 0)
+                    {
+                        contador++;
+                    }
+                }
+            }
+            Console.WriteLine("Quantidade de números negativos: " + contador);
+        }
+    }
+}
---
+++ Array/propriedadesMatriz/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace propriedadesMatriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instancia na memoria uma matriz de duas linhas e 3 colunas
+            double[,] matriz = new double[2, 3];
+
+            Console.WriteLine("Quantidade de elementos na matriz: " + matriz.Length);
+
+            Console.WriteLine("Quantidade de linhas da matriz: " + matriz.Rank);
+
+            Console.WriteLine("Tamanho da primeira dimensão (linhas): " + matriz.GetLength(0));
+            Console.WriteLine("Tamanho da segunda dimensão (colunas): " + matriz.GetLength(1));
+        }
+    }
+}
