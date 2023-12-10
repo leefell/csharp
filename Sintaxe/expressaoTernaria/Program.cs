e5e5c5cb9735e06e09311b3254fa63bb133f6762
leefell <alexandreaug21@gmail.com>
2023-12-10 15:13:35 -0300

    expressao ternaria

---
+++ Sintaxe/expressaoTernaria/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+namespace expressaoTernaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+            //Preço é menor que 20? se sim recebe desconto de 10%, se não recebe um de 5;
+
+            Console.WriteLine(desconto);
+
+        }
+    }
+}
