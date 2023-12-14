d899a547101b778aa44ab40162def91695052292
leefell <alexandreaug21@gmail.com>
2023-12-14 11:22:19 -0300

    another ex

---
+++ Composition/composition2/Program.cs
@@ -0,0 +1,25 @@
+﻿using composition2.Entities;
+
+namespace composition2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("That's awesome!");
+            Post p1 = new Post(DateTime.Parse("21/12/2018 13:05:44"), "Travelling to New Zeeland", "I'm going to visit this country!", 12);
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good Night!");
+            Comment c4 = new Comment("May the Force be with you!");
+            Post p2 = new Post(DateTime.Now, "Good night guys", "See you tomorrow!", 5);
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
