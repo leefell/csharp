db960b91468f6354125f58ca95d455c1b4ffa4c0
leefell <alexandreaug21@gmail.com>
2023-12-18 09:19:33 -0300

    Adding override

---
+++ Orientação à Objetos/Override/Entities/SavingsAccount.cs
@@ -0,0 +1,29 @@
+﻿namespace ex_Override.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        // Reimplementa na subclasse aquela operacao da superclasse e da um significado diferente à ela
+        public override void Withdraw(double amount)
+        {
+            base.Withdraw(amount); // efetua o saque conforma a logica implementada na superclasse por causa do base
+            Balance -= 2.0; // desconto
+        }
+    }
+}
---
+++ Orientação à Objetos/Override/Program.cs
@@ -0,0 +1,21 @@
+﻿using ex_Override.Entities;
+
+namespace ex_Override
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc1 = new Account(1001, "Alex", 500);
+            Account acc2 = new SavingsAccount(1002, "Anna", 500, 0.01);
+
+            acc1.Withdraw(10.0);
+            acc2.Withdraw(10.0);
+
+            // Conta comum taxa 5.0 | Conta poupança taxa 2.0 
+
+            Console.WriteLine(acc1.Balance);
+            Console.WriteLine(acc2.Balance);
+        }
+    }
+}
