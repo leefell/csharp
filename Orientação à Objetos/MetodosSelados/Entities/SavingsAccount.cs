c8433403b89e1ba5f7fec3a0cc66156a9253205e
leefell <alexandreaug21@gmail.com>
2023-12-18 10:04:27 -0300

    sealed

---
+++ Orientação à Objetos/MetodosSelados/Entities/SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿namespace MetodosSelados.Entities
+{
+    // Com o sealed, a classe SavinsAccount não pode ter subclasses
+    sealed internal class SavingsAccount : Account
+    {
+        public double InterestRate { get; set; }
+
+        public SavingsAccount()
+        {
+
+        }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance()
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        // Essa operação não pode ser sobrescrita novamente em uma outra subclasse, caso a classe nao seja selada
+        public sealed override void Withdraw(double amount)
+        {
+            base.Withdraw(amount); // efetua o saque conforma a logica implementada na superclasse por causa do base
+            Balance -= 2.0; // desconto
+        }
+    }
+}
