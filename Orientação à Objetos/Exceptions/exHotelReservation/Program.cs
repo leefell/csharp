10ec75d708c966b8c0ad3612d4bdcbd2ede722d1
leefell <alexandreaug21@gmail.com>
2023-12-21 10:43:46 -0300

    exception exercise

---
+++ Orientação à Objetos/Exceptions/exHotelReservation/Program.cs
@@ -0,0 +1,48 @@
+﻿using exHotelReservation.Entities;
+using exHotelReservation.Entities.Exceptions;
+
+namespace exHotelReservation
+{
+    // Nota: quando for executar use uma data posterior ao dia da compilacao, a funcao DateTime.Now vai pegar o horario do sistema
+    // entao o input for uma data passada ele sempre vai lancar a exceção
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room number: ");
+                int number = int.Parse(Console.ReadLine());
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                Reservation reservation = new Reservation(number, checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+
+                Console.WriteLine("\nEnter data to update the reservation:");
+                Console.Write("Check-in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+                Console.Write("Check-out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                reservation.UpdateDates(checkIn, checkOut);
+                Console.WriteLine("Reservation: " + reservation);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Error in format: " + e.Message);
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error in reservation: " + e.Message);
+            }
+            catch (Exception e) // para pegar qualquer outra exceção, esse "Exception" é a maneira mais generica de exceção.
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
