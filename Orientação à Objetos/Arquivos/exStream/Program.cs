e3cc031eeec845f19adf70a14a7f439b9ed91929
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2023-12-29 12:10:45 -0300

    Usando stream e suas propriedades

---
+++ Orientação à Objetos/Arquivos/exStream/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace exStream
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\leele\Documents\GitHub\csharp\Orientação à Objetos\Arquivos\Textos\e2\text1.txt";
+            StreamReader sr = null;
+
+            try
+            {
+                sr = File.OpenText(path); //Implicito
+                // Explicito = sr = new StreamReader(fs);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error ocurred.");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                if (sr != null) sr.Close();
+            }
+        }
+    }
+}
