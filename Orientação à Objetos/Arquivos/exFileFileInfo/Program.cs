ead9631fe8be67a334324a85b4d540bb3c76aac9
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2023-12-29 11:50:57 -0300

    Trabalhando com arquivos

---
+++ Orientação à Objetos/Arquivos/exFileFileInfo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+
+namespace exFileFileInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // colocando o '@' não precisa colocar duas barras \\leele\\Documents\\etc
+            string sourcePath = @"C:\Users\leele\Documents\GitHub\csharp\Orientação à Objetos\Arquivos\Textos\text1.txt";
+            string targetPath = @"C:\Users\leele\Documents\GitHub\csharp\Orientação à Objetos\Arquivos\Textos\text2.txt";
+
+            try
+            {
+
+                FileInfo fileInfo = new FileInfo(sourcePath);
+                fileInfo.CopyTo(targetPath);
+                string[] lines = File.ReadAllLines(sourcePath);
+                foreach(string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An erro ocurred.");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
