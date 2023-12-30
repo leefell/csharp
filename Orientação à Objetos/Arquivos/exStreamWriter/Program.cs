75c3912ef413da832ca77df99f9af74169968eaf
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2023-12-29 21:20:29 -0300

    StreamWriter

---
+++ Orientação à Objetos/Arquivos/exStreamWriter/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace exStreamWriter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourcePath = @"C:\Users\leele\Documents\GitHub\csharp\Orientação à Objetos\Arquivos\Textos\e4\text1.txt";
+            string targetPath = @"C:\Users\leele\Documents\GitHub\csharp\Orientação à Objetos\Arquivos\Textos\e4\text2.txt";
+            try
+            {
+                string[] lines = File.ReadAllLines(sourcePath);
+                using (StreamWriter sw = File.AppendText(targetPath))
+                {
+                    foreach (string line in lines)
+                    {
+                        sw.WriteLine(line.ToUpper());
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("An error occurred");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
