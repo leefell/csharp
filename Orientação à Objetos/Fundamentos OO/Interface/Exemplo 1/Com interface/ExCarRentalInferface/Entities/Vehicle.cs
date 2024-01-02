0c1ebf57396a0e976175a3255d789dcad48e7b2f
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-02 09:44:41 -0300

    Solução com interface

---
+++ Orientação à Objetos/Fundamentos OO/Interface/Exemplo 1/Com interface/ExCarRentalInferface/Entities/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace ExCarRentalInferface.Entities
+{
+    internal class Vehicle
+    {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
