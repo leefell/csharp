232bf99953624ca826ef5ef8ebde4cee2706f464
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-05 10:54:32 -0300

    Ordenando a lista de nomes

---
+++ Orientação à Objetos/Fundamentos OO/Interface/interfaceComparable/Entities/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Entities
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string csvEmployee)
+        {
+            string[] vetor = csvEmployee.Split(',');
+            Name = vetor[0];
+            Salary = double.Parse(vetor[1], CultureInfo.InvariantCulture);
+        }
+
+        public override string ToString()
+        {
+            return Name + ", " + Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Orientação à Objetos/Fundamentos OO/Interface/interfaceComparable/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace interfaceComparable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\leele\Documents\GitHub\csharp\Orientação à Objetos\Fundamentos OO\Interface\nomes.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    List<string> lista = new List<string>();
+                    while (!sr.EndOfStream)
+                    {
+                        lista.Add(sr.ReadLine());
+                    }
+                    lista.Sort(); // ordernar lista
+                    foreach(string s in lista)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error :/");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
