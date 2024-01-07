bee69559beac041e428e5c800a96881f3798ce91
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-07 19:45:15 -0300

    Input without generic

---
+++ Orientação à Objetos/Técnicas/Generics, Set & Dictionary/restricoesGenerics/Program.cs
@@ -0,0 +1,27 @@
+﻿using Services;
+
+namespace restricoesGenerics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+
+            Console.WriteLine("Input the number of products that will be registered: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < n; i++)
+            {
+                int x = int.Parse(Console.ReadLine());
+                list.Add(x);
+            }
+
+            CalculationService calculationService = new CalculationService();
+
+            int max = calculationService.Max(list);
+
+            Console.WriteLine("Max: " + max);
+        }
+    }
+}
