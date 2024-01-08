6016647586668f78bd20d306441f31a5fb386f14
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-08 10:02:44 -0300

    Começando HashSet

---
+++ Orientação à Objetos/Técnicas/Generics, Set & Dictionary/exHashAndSortedSets/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace exHashAndSortedSets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            set.Add("Café");
+            set.Add("Notebook");
+            set.Add("Celular");
+
+            Console.WriteLine(set.Contains("Notebook"));
+            Console.WriteLine(set.Contains("Tangerina"));
+            foreach(string coisa in set)
+            {
+                Console.WriteLine("\n" + coisa);
+            }
+
+            set.Clear(); // limpa o set
+            foreach(string coisa in set)
+            {
+                Console.WriteLine("\n" + coisa); // output: null
+            }
+        }
+    }
+}
