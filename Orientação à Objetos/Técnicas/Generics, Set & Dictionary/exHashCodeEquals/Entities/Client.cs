9e55ecf2b269891ac5aa51b77e8b79f29546d6b8
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-08 09:16:01 -0300

    Aprendendo GetHashCode e Equals

---
+++ Orientação à Objetos/Técnicas/Generics, Set & Dictionary/exHashCodeEquals/Entities/Client.cs
@@ -0,0 +1,27 @@
+﻿namespace Entities
+{
+    internal class Client
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        // Um cliente é igual o outro se o email é igual
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is Client))
+            {
+                return false;
+            }
+
+            Client other = obj as Client; // downcast
+
+            return Email.Equals(other.Email);
+        }
+
+        public override int GetHashCode()
+        {
+            return Email.GetHashCode();
+        }
+    }
+}
---
+++ Orientação à Objetos/Técnicas/Generics, Set & Dictionary/exHashCodeEquals/Program.cs
@@ -0,0 +1,22 @@
+﻿using Entities;
+
+namespace exHashCodeEquals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Client a = new Client { Name = "Maria", Email = "maria@gmail.com"};
+            Client b = new Client { Name = "Alex", Email = "alex@gmail.com"};
+
+            Console.WriteLine(a.Equals(b)); // output: false // aqui o compilador vai comprar o conteudo
+
+            Console.WriteLine(a == b); // aqui o compilador vai comparar a referencia do ponteiro de memoria do obj
+
+            Console.WriteLine(a.GetHashCode());
+            Console.WriteLine(b.GetHashCode());
+
+            
+        }
+    }
+}
