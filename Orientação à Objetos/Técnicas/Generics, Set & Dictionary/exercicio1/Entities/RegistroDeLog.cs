2f89d810f342dccc070b5cf75d65c677836e3a61
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-09 11:00:59 -0300

    Exercício 1

---
+++ Orientação à Objetos/Técnicas/Generics, Set & Dictionary/exercicio1/Entities/RegistroDeLog.cs
@@ -0,0 +1,31 @@
+﻿namespace Entities
+{
+    internal class RegistroDeLog
+    {
+        public string Usuario { get; set; }
+        public DateTime Instante { get; set; }
+
+        public RegistroDeLog(string usuario, DateTime instante)
+        {
+            Usuario = usuario;
+            Instante = instante;
+        }
+
+        public override int GetHashCode()
+        {
+            return Usuario.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if(!(obj is RegistroDeLog))
+            {
+                return false;
+            }
+
+            RegistroDeLog outro = obj as RegistroDeLog;
+
+            return Usuario.Equals(outro.Usuario);
+        }
+    }
+}
---
+++ Orientação à Objetos/Técnicas/Generics, Set & Dictionary/exercicio1/Program.cs
@@ -0,0 +1,39 @@
+﻿//C:\Users\leele\Documents\GitHub\csharp\Orientação à Objetos\Técnicas\Generics, Set & Dictionary\text\in.txt
+using Entities;
+
+namespace exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("Digite o caminho completo do arquivo: ");
+            //string path = Console.ReadLine();
+
+            HashSet<RegistroDeLog> logs = new HashSet<RegistroDeLog>(); 
+
+            string path = @"C:\Users\leele\Documents\GitHub\csharp\Orientação à Objetos\Técnicas\Generics, Set & Dictionary\text\in.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string nome = line[0];
+                        DateTime instante = DateTime.Parse(line[1]);
+
+                        logs.Add(new RegistroDeLog(nome, instante));
+                    }
+                    Console.WriteLine("Total Users: " + logs.Count);
+                }
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine("Something went wrong! :/");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
