609f44fc4e0bdff6a10ac32dcd55a43e4c62e92e
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-09 20:19:39 -0300

    Exercicio Dictionary

---
+++ Orientação à Objetos/Técnicas/Generics, Set & Dictionary/exercicioDictionary/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace exercicioDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\leele\Documents\GitHub\csharp\Orientação à Objetos\Técnicas\Generics, Set & Dictionary\text\votos.txt";
+
+            try
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+
+                    Dictionary<string, int> dVotos = new Dictionary<string, int>();
+
+                    while (!sr.EndOfStream)
+                    {
+                        string[] lines = sr.ReadLine().Split(",");
+                        string name = lines[0];
+                        int votos = int.Parse(lines[1]);
+
+                        if (dVotos.ContainsKey(name))
+                        {
+                            dVotos[name] += votos;
+                        }
+                        else
+                        {
+                            dVotos[name] = votos;
+                        }
+                    }
+                    foreach (KeyValuePair<string, int> keyVotos in dVotos)
+                    {
+                        Console.WriteLine(keyVotos.Key + ": " + keyVotos.Value);
+                    }
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("Oops! Something went wrong." + e.Message);
+            }
+        }
+    }
+}
