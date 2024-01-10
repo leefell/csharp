676c94650c181bf04fc7bc2bbd760b3baf43bcfc
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-10 10:04:12 -0300

    Demo extensions methods 2

---
+++ Orientação à Objetos/Utilitários e Ferramentas/extensionMethods2/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace System
+{
+    internal static class StringExtensions
+    {
+        public static string Cut(this string thisOBJ, int contador)
+        {
+            if(thisOBJ.Length <= contador)
+            {
+                return thisOBJ;
+            }
+            else
+            {
+                return thisOBJ.Substring(0, contador) + "...";
+            }
+        }
+    }
+}
---
+++ Orientação à Objetos/Utilitários e Ferramentas/extensionMethods2/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace extensionMethods2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "Good morning Guys! i've been coding for almost 1 year!";
+            Console.WriteLine(s1.Cut(10));
+        }
+    }
+}
