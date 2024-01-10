5c82866df18923d18689d6dd565a57883acebcca
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-10 09:54:49 -0300

    Demo extension methods

---
+++ Orientação à Objetos/Utilitários e Ferramentas/extensionMethods/Extensions/DateTimeExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Globalization;
+
+namespace Extensions
+{
+    internal static class DateTimeExtensions
+    {
+        public static string ElapsedTime(this DateTime thisOBJ)
+        {
+            TimeSpan duration = DateTime.Now.Subtract(thisOBJ);
+
+            if(duration.TotalHours < 24.0)
+            {
+                return duration.TotalHours.ToString("F1", CultureInfo.InvariantCulture) + " hours.";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1", CultureInfo.InvariantCulture) + " days.";
+            }
+        }
+    }
+}
---
+++ Orientação à Objetos/Utilitários e Ferramentas/extensionMethods/Program.cs
@@ -0,0 +1,13 @@
+﻿using Extensions;
+
+namespace extensionMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+            Console.WriteLine(dt.ElapsedTime());
+        }
+    }
+}
