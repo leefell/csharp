478a9c36e5894a6726539fbbd3aef5ca8e080f55
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-11 10:12:51 -0300

    Multidelegates

---
+++ Orientação à Objetos/Técnicas/Lambda, delegates & LINQ/demoMultiDelegates/Program.cs
@@ -0,0 +1,20 @@
+﻿using Services;
+
+namespace demoMultiDelegates
+{
+    delegate void BinaryNumericOperation(double n1, double n2);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.ShowSum;
+            op += CalculationService.ShowMax; // agr o delegate guarda ref para duas funcoes
+
+            op.Invoke(a, b);
+            //op(a, b);
+        }
+    }
+}
