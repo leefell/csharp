6435cdd00d9c6b584b3f1bfee0c6a02b8b316ea3
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-11 10:05:43 -0300

    Demo Delegates

---
+++ Orientação à Objetos/Técnicas/Lambda, delegates & LINQ/demoDelegate1/Program.cs
@@ -0,0 +1,21 @@
+﻿using Services;
+
+namespace demoDelegate1
+{
+    delegate double BinaryNumericOperation(double n1, double n2);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 10;
+            double b = 12;
+
+            BinaryNumericOperation op = CalculationService.Sum;
+            //BinaryNumericOperation op = new BinaryNumericOperation(CalculationService.Sum);
+
+            double result = op(a, b);
+            //double result = op.Invoke(a, b);
+            Console.WriteLine(result);
+        }
+    }
+}
