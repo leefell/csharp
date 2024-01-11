279b07040948a477e4e99022e76cb375c36a5a72
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-11 09:56:22 -0300

    Funcao NAO referencialmente transparente

---
+++ Orientação à Objetos/Técnicas/Lambda, delegates & LINQ/demoTransparenciaReferencial/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace demoNaoTransparenciaReferencial
+{
+    internal class Program
+    {
+
+        public static int globalValue = 3;
+
+        static void Main(string[] args)
+        {
+            int[] vect = new int[] { 3, 4, 5 };
+            ChangeOddValues( vect );
+            Console.WriteLine(string.Join(" ", vect));
+
+        }
+
+        public static void ChangeOddValues(int[] numbers)
+        {
+
+            // Essa funcao nao é referencialmente transparente porque é necessário olhar fora do 
+            // escopo dela para saber oq ela faz, uma vez que o 'globalValue' esta fora dela;
+
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 != 0)
+                {
+                    numbers[i] += globalValue;
+                }
+            } 
+        }
+    }
+}
