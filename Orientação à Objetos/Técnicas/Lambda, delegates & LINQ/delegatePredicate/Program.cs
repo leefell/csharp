625d0fa38afe72573b3cfe288be9f0537d5797c0
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-12 09:25:38 -0300

    Delegate Predicate

---
+++ Orientação à Objetos/Técnicas/Lambda, delegates & LINQ/delegatePredicate/Program.cs
@@ -0,0 +1,31 @@
+﻿using Entities;
+
+namespace delegatePredicate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Quero remover aqueles produtos que custam 100 ou mais
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD case", 80.90));
+
+            //list.RemoveAll(p => p.Price >= 100.0); // lambda
+            list.RemoveAll(ProductTest);
+            foreach(Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }
+
+        static bool ProductTest(Product p)
+        {
+            return p.Price >= 100.0;
+        }
+    }
+}
