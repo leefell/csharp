389c9dbbd3318523f80c0c7d8d6d9bb12bfe8adf
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-12 09:43:32 -0300

    Delegate Func

---
+++ Orientação à Objetos/Técnicas/Lambda, delegates & LINQ/delegateFunc/Program.cs
@@ -0,0 +1,36 @@
+﻿using Entities;
+
+namespace delegateFunc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // gerar uma nova lista com os nomes dos produtos em CAPS
+
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.00));
+            list.Add(new Product("HD case", 80.90));
+
+            Func<Product, string> func = NameUpper;
+            //Func<Product, string> func = p => p.Name.ToUpper(); // lambda
+
+            List<string> resultado = list.Select(func).ToList();
+            //List<string> resultado = list.Select(p => p.Name.ToUpper()).ToList(); // lambda 2
+            //List<string> resultado = list.Select(NameUpper).ToList(); // nesse caso nao precisa do delegate
+            foreach(string s in resultado)
+            {
+                Console.WriteLine(s);
+            }
+        }
+
+        static string NameUpper(Product p)
+        {
+            return p.Name.ToUpper();
+        }
+    }
+}
