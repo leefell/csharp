d84c8a94d5e7ef7a295dc158b9971f2e758b587c
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-12 19:17:29 -0300

    Exercicio 1

---
+++ Orientação à Objetos/Técnicas/Lambda, delegates & LINQ/exercicio1/Program.cs
@@ -0,0 +1,36 @@
+﻿using Entities;
+using System.Globalization;
+
+namespace exercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\leele\Documents\GitHub\csharp\Orientação à Objetos\Técnicas\Lambda, delegates & LINQ\texts\arq1.txt";
+
+            List<Product> list = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while(!sr.EndOfStream)
+                {
+                    string[] campos = sr.ReadLine().Split(",");
+                    string name = campos[0];
+                    double price = double.Parse(campos[1], CultureInfo.InvariantCulture);
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            var avg = list.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+            Console.WriteLine("Average price = " + avg.ToString("F2", CultureInfo.InvariantCulture));
+
+            var names = list.Where(p => p.Price < avg).OrderByDescending(p => p.Name).Select(p => p.Name); // expressao LINQ completa
+
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
