1f98afc93ae18764919113eeca438cfdd043f7c1
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-01-12 10:18:18 -0300

    Introdução ao LINQ

---
+++ Orientação à Objetos/Técnicas/Lambda, delegates & LINQ/demoLINQ1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace demoLINQ1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Especificar o data source
+            int[] numbers = new int[] { 2, 3, 4, 5 };
+
+            // Define the query expression (consulta)
+            IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+            // ou var result = (...)
+
+            // Execute the query
+            foreach (int x in result)
+            {
+                Console.Write(x + " ");
+            }
+        }
+    }
+}
