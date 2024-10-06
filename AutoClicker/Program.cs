a922027701ef1f582b0bf228a5d4183fe3d9ad3a
alexandreaug21@gmail.com <alexandreaug21@gmail.com>
2024-10-06 16:57:49 -0300

    autoclicker

---
+++ AutoClicker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+[DllImport("user32.dll")]
+static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, IntPtr dwExtraInfo); // click
+
+[DllImport("user32.dll")]
+static extern short GetAsyncKeyState(int vKey); // hotkey
+
+const uint LEFTDOWN = 0x02;
+const uint LEFTUP = 0x04;
+const int HOTKEY = 0x26; // seta pra cima
+
+bool enableClicker = false;
+int clickInterval = 5;
+
+while (true)
+{
+    if(GetAsyncKeyState(HOTKEY) < 0)
+    {
+        enableClicker = !enableClicker;
+        Thread.Sleep(300);
+    }
+
+    if (enableClicker)
+    {
+        MouseClick();
+    }
+    Thread.Sleep(clickInterval);
+}
+
+void MouseClick()
+{
+    mouse_event(LEFTDOWN, 0, 0, 0, IntPtr.Zero);
+
+    mouse_event(LEFTUP, 0, 0, 0, IntPtr.Zero);
+}
